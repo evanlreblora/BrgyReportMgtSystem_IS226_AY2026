f9d26246d0587adc970da189c4ecbc74af26014c
darkcrayola <evan.reblora@gmail.com>
2026-03-26 07:46:44 +0800

    Initial commit

---
+++ header.php
@@ -0,0 +1,35 @@
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand-md " style="background-color: #213C51">
+    <div class="container">
+      <a href="" class="navbar-brand">
+        <img src="assets/dist/img/<?= $image  ?>" alt="logo" class="brand-image img-circle " >
+        <span class="brand-text  text-white"  style="font-weight: 700">BARANGAY PORTAL</span>
+      </a>
+
+      <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse order-3" id="navbarCollapse">
+        <!-- Left navbar links -->
+
+
+       
+      </div>
+
+      <!-- Right navbar links -->
+      <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto " >
+          <li class="nav-item">
+            <a href="index.php" class="nav-link text-white rightBar" style="  border-bottom: 3px solid red;"><i class="fas fa-home"></i> HOME</a>
+          </li>
+          <li class="nav-item">
+            <a href="register.php" class="nav-link text-white rightBar"><i class="fas fa-user-plus"></i> REGISTER</a>
+          </li>
+          <li class="nav-item">
+            <a href="login.php" class="nav-link text-white rightBar"><i class="fas fa-user-alt"></i> LOGIN</a>
+          </li>
+      </ul>
+    </div>
+  </nav>
+  <!-- /.navbar -->
---
+++ resident/footer.php
@@ -0,0 +1,16 @@
+  <footer class="main-footer text-white" style="background-color: #6594B1">
+    <div class="float-right d-none d-sm-block">
+<?php
+    // Create a DateTime object with the desired timezone (GMT+7)
+    try {
+        $date = new DateTime("now", new DateTimeZone("GMT+7"));
+        echo "Date and Time: " . $date->format("Y-m-d | H:i:s") . "\n";
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+    ?>
+
+    </div>
+    <i class="fas fa-map-marker-alt"></i> <?= $postal_address ?> 
+  </footer>
+ 
